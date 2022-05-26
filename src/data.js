9b0d5d326bad6841aff16db3fe6d9ab708a7bfa7
Mario Magomedov <webdevelope2017@gmail.com>
2022-05-26 11:55:40 -0500

    data for extras

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const extraData = [
+	{
+		title: 'Deep Cleaning 2.7x Price',
+		img: '',
+		text: 'If your home has NOT been maintained and/or cleaned by you or any professional in the last 30 days, we require more time and attention to guarantee your satisfaction.',
+		price: '',
+	},
+	{
+		title: 'Laundry',
+		img: '',
+		text: 'We wash, dry, and fold your clothes [bed sheets, towels...] during your scheduled cleaning. We can usually do 2-3 full loads, depending on how long we are scheduled.',
+		price: '$11 per load',
+	},
+	{
+		title: 'Bed Sheets',
+		img: '',
+		text: 'We freshen up your beds by changing the sheets and pillow cases for each one of your beds. This price is per bed and should be scheduled in advance.',
+		price: '$6 per bed',
+	},
+	{
+		title: 'Basement',
+		img: '',
+		text: 'Basement for 2-3 bed homes $25;  4 and up bedrooms  $33.',
+		price: '$25-$33',
+	},
+	{
+		title: 'Inside Oven',
+		img: '',
+		text: 'We use the best products available on the market to clean your oven. For best results, use the self-cleaning function prior to our visit.',
+		price: '$25',
+	},
+	{
+		title: 'Inside Refrigerator',
+		img: '',
+		text: 'Our maids will remove the food from the fridge, clean the shelves, the walls, and the interior of your appliance.',
+		price: '$30',
+	},
+]
