d14eb012aa9770e19a9bda1a639649bd26f1896b
Mario Magomedov <webdevelope2017@gmail.com>
2022-05-27 01:18:43 -0500

    update emailjs

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -4,14 +4,13 @@ import TextField from '@mui/material/TextField'
 import { FormControl } from '@mui/material'
 import emailjs from '@emailjs/browser'
 
-import { PHONE, ADDRESS, EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, EMAILJS_PUBLIC_KEY } from '../utils/constants'
+import { PHONE, ADDRESS, EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_CONTACT_ID, EMAILJS_PUBLIC_KEY } from '../utils/constants'
 
 export default function FeedbackForm() {
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
 	const [phone, setPhone] = useState('')
 	const [message, setMessage] = useState('')
-	const [address, setAddress] = useState('')
 	const form = useRef()
 
 	const sendEmail = (e) => {
@@ -23,22 +22,21 @@ export default function FeedbackForm() {
 			const fieldsData = {
 				name,
 				email,
-				address,
 				phone,
 				message,
 			}
 
-			emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, fieldsData, EMAILJS_PUBLIC_KEY).then(
+			emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_CONTACT_ID, fieldsData, EMAILJS_PUBLIC_KEY).then(
 				(result) => {
 					// console.log('emailjs result', result)
 					// console.log('emailjs', result.text)
 
 					if (result.status === 200) {
-						alert('Success, See you soon')
 						setName('')
 						setEmail('')
 						setPhone('')
 						setMessage('')
+						alert('Success, See you soon')
 					} else {
 						alert('Oops, something was wrong! please try again or call me')
 					}
@@ -60,47 +58,45 @@ export default function FeedbackForm() {
 					{ADDRESS}
 				</Typography>
 			</Grid>
-			<FormControl ref={form}>
-				<Grid container sm={12} item={true} justifyContent='center'>
-					<Grid
-						style={{ paddingTop: '50px', paddingBottom: '100px' }}
-						container
-						justifyContent='center'
-						spacing={2}
-						md={8}
-						item={true}>
-						<Grid item={true} xs={12} sm={6} md={4}>
-							<TextField label='Name' value={name} onChange={(e) => setName(e.target.value)} fullWidth />
-						</Grid>
-						<Grid item={true} xs={12} sm={6} md={4}>
-							<TextField required label='Phone*' value={phone} onChange={(e) => setPhone(e.target.value)} fullWidth />
-						</Grid>
-						<Grid item={true} xs={12} sm={6} md={4}>
-							<TextField required label='Email*' value={email} onChange={(e) => setEmail(e.target.value)} fullWidth />
-						</Grid>
+			<Grid container sm={12} item={true} justifyContent='center'>
+				<Grid
+					style={{ paddingTop: '50px', paddingBottom: '100px' }}
+					container
+					justifyContent='center'
+					spacing={2}
+					md={8}
+					item={true}>
+					<Grid item={true} xs={12} sm={6} md={6}>
+						<TextField label='Name' value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+					</Grid>
+					<Grid item={true} xs={12} sm={6} md={6}>
+						<TextField required label='Phone*' value={phone} onChange={(e) => setPhone(e.target.value)} fullWidth />
+					</Grid>
+					<Grid item={true} xs={12} sm={6} md={12}>
+						<TextField required label='Email*' value={email} onChange={(e) => setEmail(e.target.value)} fullWidth />
+					</Grid>
 
-						<Grid item={true} xs={12}>
-							<TextField
-								multiline
-								rows={4}
-								fullWidth
-								required
-								label='Message*'
-								value={message}
-								onChange={(e) => {
-									// console.log(newValue?.toLocaleTimeString())
-									setMessage(e.target.value)
-								}}
-							/>
-						</Grid>
-						<Grid style={{ marginTop: '20px' }} container xs={12} item={true} justifyContent='center'>
-							<Button onClick={(e) => sendEmail(e)} variant='outlined'>
-								Send
-							</Button>
-						</Grid>
+					<Grid item={true} xs={12}>
+						<TextField
+							multiline
+							rows={4}
+							fullWidth
+							required
+							label='Message*'
+							value={message}
+							onChange={(e) => {
+								// console.log(newValue?.toLocaleTimeString())
+								setMessage(e.target.value)
+							}}
+						/>
+					</Grid>
+					<Grid style={{ marginTop: '20px' }} container xs={12} item={true} justifyContent='center'>
+						<Button onClick={(e) => sendEmail(e)} variant='outlined'>
+							Send
+						</Button>
 					</Grid>
 				</Grid>
-			</FormControl>
+			</Grid>
 		</>
 	)
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,9 @@
-export const BRAND_NAME = 'Cleaning maids service'
+export const BRAND_NAME = 'Illinois Maids'
 export const PHONE = '+1 847 312 5422'
 export const ADDRESS = 'Park Ridge / Edison Park / Norwood Park / Morton Grove / Niles / Norridge / Rosemont'
 
-export const EMAILJS_SERVICE_ID = 'service_k93fxgq'
+export const EMAILJS_SERVICE_ID = 'service_2etjb4f'
 export const EMAILJS_TEMPLATE_ID = 'template_xja5pgn'
+export const EMAILJS_TEMPLATE_CONTACT_ID = 'template_ebyih3g'
 export const EMAILJS_PUBLIC_KEY = 'AEc9makvcDWxCV9nf'
 export const EMAILJS_PRIVATE_KEY = 'SnxIGhG_JCKtV29MPEoQJ'
