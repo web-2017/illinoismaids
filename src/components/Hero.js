cdd00c442426f24c30b05914e8536a94588f29b5
Mario Magomedov <webdevelope2017@gmail.com>
2022-05-27 21:18:22 -0500

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
+import { Box } from '@mui/material'
 import { styled } from '@mui/system'
 import Typography from '@mui/material/Typography'
 
@@ -14,23 +15,26 @@ const StyledBackground = styled(Container, {
 	marginTop: '50px',
 	padding: '25px',
 	height: '500px',
-	background: `url(${bgHero})`,
-	backgroundRepeat: 'no-repeat',
-	backgroundSize: '100% 100%',
-	backgroundAttachment: 'fixed',
+	background: `url(${bgHero}) no-repeat top center fixed`,
+	backgroundSize: 'contain',
+	justifyContent: 'center',
+	display: 'flex',
+	alignItems: 'center',
 })
 export const Hero = () => {
 	return (
-		<StyledBackground
-			sx={{
-				bgcolor: 'background.paper',
-				pt: 8,
-				pb: 6,
-			}}>
-			<Container maxWidth='sm'>
-				<Typography variant='h4'>Welcome to your ECO Home</Typography>
-				<BookBtn />
-			</Container>
+		<StyledBackground>
+			<Box style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
+				<Typography
+					variant='h4'
+					align='center'
+					sx={{ pb: 4 }}
+					style={{ color: '#fff', textShadow: ' 5px 4px 5px #000' }}>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus quo delectus, hic autem laborum nam aut
+					dolor impedit debitis asperiores?
+				</Typography>
+				<BookBtn variant='contained' />
+			</Box>
 		</StyledBackground>
 	)
 }
